5759ef3c186e541493278d1d0d315a3a03672bdc
dyatskovskiy <d.yatskovskiy@ukr.net>
2025-03-18 18:00:09 +0200

    Fix loader error

--- src/app/components/LoaderOverlay.tsx
+++ src/app/components/LoaderOverlay.tsx
@@ -1,8 +1,12 @@
+"use client";
+
 import React from "react";
 import { Bars } from "react-loader-spinner";
 import { createPortal } from "react-dom";
 
 export const LoaderOverlay = () => {
+  if (typeof window === "undefined") return null;
+
   return createPortal(
     <div className="absolute top-0 left0 z-[10000] w-screen h-screen flex flex-col justify-center items-center bg-black bg-opacity-40">
       <Bars
@@ -14,6 +18,6 @@ export const LoaderOverlay = () => {
       />
       <p className="mt-2">Please, wait a moment...</p>
     </div>,
-    document.body,
+    window.document.body,
   );
 };
